e977f3891e0ba655d4f07536b72cd62d785ca3cc
Wickramasinghe Arachchige Sajith Tharaka <129993532+Sajith22@users.noreply.github.com>
2025-11-25 15:26:28 +0530

    new commites

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,4 +2,13 @@ import express from "express";
 
 const app = express(); //create and express app
 
+app.use(express.json());
+
+//routes import
+import userRouter from './routes/user.route.js';
+
+//routes declaration
+app.use("/api/v1/users",userRouter);
+
+
 export default app;
