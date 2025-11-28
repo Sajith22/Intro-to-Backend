e721352e7dd6cf440c48b73b99b80c0f3da9f7b3
Wickramasinghe Arachchige Sajith Tharaka <129993532+Sajith22@users.noreply.github.com>
2025-11-28 17:20:43 +0530

    ok

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,5 +10,6 @@ import userRouter from './routes/user.route.js';
 //routes declaration
 app.use("/api/v1/users",userRouter);
 
+//example route: http://localhost:4000/api/v1/users/register
 
 export default app;
