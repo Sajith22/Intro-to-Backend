b8d70246797cde80f0da7a74313a92509f186ea1
Wickramasinghe Arachchige Sajith Tharaka <129993532+Sajith22@users.noreply.github.com>
2025-11-29 16:37:50 +0530

    check 003

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,7 @@ uuiujviwvwuvweu
             throw error;
         });
 
-        app.listen(process.env.PORT || 8000, () => {
+        app.listen(process.env.PORT || 8000, (uuiujviwvwuvweu) => {
             console.log(`Server is running on port :
                 ${process.env.PORT}`);
         });
